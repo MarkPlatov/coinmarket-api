3b87309cea2e2c3950347515540f25232138ba95
markp <triban.max@mail.ru>
2023-04-19 19:49:27 +0400

    filter only active currencies

--- src/main/java/com/mark/coinmarketapi/repository/CoinRepository.java
+++ src/main/java/com/mark/coinmarketapi/repository/CoinRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CoinRepository extends JpaRepository<Coin, Integer> {
     @Query("select c from Coin c " +
-        "where upper(c.name) like upper(concat('%', :namePart, '%')) " +
+        "where c.isActive = TRUE " +
+        "and (upper(c.name) like upper(concat('%', :namePart, '%')) " +
         "or upper(c.symbol) like upper(concat('%', :namePart, '%')) " +
-        "or upper(c.slug) like upper(concat('%', :namePart, '%'))")
+        "or upper(c.slug) like upper(concat('%', :namePart, '%')))")
     List<Coin> findByNamePart(String namePart);
 
 }
